46277f22e8e0c3d01067ae2c15a539d41919d790
Maxim Nikolsky <maxi.nikolsky@gmail.com>
2015-11-25 15:47:18 +0300

    Implemented Socket class

--- server/Socket.cpp
+++ server/Socket.cpp
@@ -1,1 +1,27 @@
 #include "Socket.h"
+
+Socket::Socket(int socketDescriptor) {
+    this->socketDescriptor_ = socketDescriptor;
+}
+
+Socket::~Socket() {
+    close(socketDescriptor_);
+}
+
+int Socket::getSocketDescriptor() {
+    return socketDescriptor_;
+}
+
+void Socket::writeData(std::string data) {
+    int readSize = 0;
+    while (readSize < data.size())
+        readSize += write(socketDescriptor_, &readSize, data.size() - readSize);
+}
+
+std::string Socket::readData(size_t outSize) {
+    std::string outString;
+    outString.resize(outSize);
+    ssize_t size = read(socketDescriptor_, const_cast<char *>(outString.c_str()), outSize);
+    outString.resize(size);
+    return outString;
+}
